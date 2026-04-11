36dcb7dd6f9a52b8452c02f76d497c54aef4f21c
Axiom <contidavide971@gmail.com>
2026-04-11 09:40:23 +0000

    refactor

--- src/main.c
+++ src/main.c
@@ -5,12 +5,14 @@
 #include "neuralNetwork.h"
 #include "utils.h"
 
-int main() {
-    Sample *trainData = loadSamples("resources/data/train-images.idx3-ubyte",
+static DatasetData buildDatasetData(Sample *trainData, Sample *testData);
+
+static NeuralNetworkParameters buildNeuralNetworkParameters(int numEpochs, int miniBatchLenght, double learningRate);
+ 
+int main() { Sample *trainData = loadSamples("resources/data/train-images.idx3-ubyte",
             "resources/data/train-labels.idx1-ubyte");
     Sample *testData =  loadSamples("resources/data/t10k-images.idx3-ubyte",
             "resources/data/t10k-labels.idx1-ubyte");
-
     normalizeImages(trainData);
     normalizeImages(testData);
 
@@ -20,16 +22,8 @@ int main() {
     NeuralNetwork *nn = createNeuralNetwork(3, 784, 30, 10);
     printf("build nn end\n");
 
-    DatasetData datasetData; 
-    datasetData.trainData = trainData;
-    datasetData.testData = testData;
-    datasetData.datasetOperation = datasetOperation;
-
-    NeuralNetworkParameters nnParameters;
-    nnParameters.numEpochs = 1;
-    nnParameters.miniBatchLength = 1;
-    nnParameters.learningRate = 1;
-
+    DatasetData datasetData = buildDatasetData(trainData, testData);
+    NeuralNetworkParameters nnParameters = buildNeuralNetworkParameters(1, 1, 1);
     train(nn, &datasetData, &nnParameters);
 
     //printSamples(trainData);
@@ -40,5 +34,23 @@ int main() {
     freeNeuralNetwork(&nn);
 
     return 0;
+}
+
+static DatasetData buildDatasetData(Sample *trainData, Sample *testData) {
+    DatasetData datasetData; 
+    datasetData.trainData = trainData;
+    datasetData.testData = testData;
+    datasetData.datasetOperation = datasetOperation;
+
+    return datasetData;
+}
+
+static NeuralNetworkParameters buildNeuralNetworkParameters(int numEpochs, int miniBatchLength, double learningRate) {
+    NeuralNetworkParameters nnParameters;
+    nnParameters.numEpochs = numEpochs;
+    nnParameters.miniBatchLength = miniBatchLength;
+    nnParameters.learningRate = learningRate;
+
+    return nnParameters;
 }
 
